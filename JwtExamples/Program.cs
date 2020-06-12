6752f809665dc5bc9ccf1dc022341a06920175b0
Bruno Brito <bhdebrito@gmail.com>
2020-06-12 01:28:04 -0300

    alfa

---
+++ JwtExamples/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.IO;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JwtExamples
+{
+    class Program
+    {
+        private static RandomNumberGenerator Rng = RandomNumberGenerator.Create();
+        private static readonly string MyJwkLocation = Path.Combine(Environment.CurrentDirectory, "mysupersecretkey.json");
+
+        static void Main(string[] args)
+        {
+            WorstExample();
+            GoodExample();
+            BestOfAllTimeJwt();
+        }
+
+        private static byte[] GenerateKey(int bytes)
+        {
+            var data = new byte[bytes];
+            Rng.GetBytes(data);
+            return data;
+        }
+
+        private static void BestOfAllTimeJwt()
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Loadkey();
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, "bruno.brito"),
+                    new Claim(ClaimTypes.Role, "admin")
+                }),
+                Expires = DateTime.UtcNow.AddHours(2),
+                SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            Console.WriteLine(tokenHandler.WriteToken(token));
+        }
+
+        private static SecurityKey Loadkey()
+        {
+            if (File.Exists(MyJwkLocation))
+                JsonSerializer.Deserialize<JsonWebKey>(File.ReadAllText(MyJwkLocation));
+
+            var newKey = CreateJWK();
+            File.WriteAllText(MyJwkLocation, JsonSerializer.Serialize(newKey));
+            return newKey;
+        }
+
+        private static JsonWebKey CreateJWK()
+        {
+            var symetricKey = new HMACSHA256(GenerateKey(64));
+            var jwk = JsonWebKeyConverter.ConvertFromSymmetricSecurityKey(new SymmetricSecurityKey(symetricKey.Key));
+            jwk.KeyId = Base64UrlEncoder.Encode(GenerateKey(16));
+            return jwk;
+        }
+        private static void GoodExample()
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = new SymmetricSecurityKey(GenerateKey(64));
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, "bruno.brito"),
+                    new Claim(ClaimTypes.Role, "admin")
+                }),
+                Expires = DateTime.UtcNow.AddHours(2),
+                SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            Console.WriteLine(tokenHandler.WriteToken(token));
+        }
+        private static void WorstExample()
+        {
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("MinhaChaveSecreta-Que-Nao-Foi-Auto-Gerada");
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, "bruno.brito"),
+                    new Claim(ClaimTypes.Role, "admin")
+                }),
+                Expires = DateTime.UtcNow.AddHours(2),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            Console.WriteLine(tokenHandler.WriteToken(token));
+        }
+    }
+}
